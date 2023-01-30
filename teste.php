d729ebc96f48d0696b4131f7b7f46074f3387184
IannOli <100435787+iannoliver@users.noreply.github.com>
2023-01-30 17:13:49 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -6,14 +6,13 @@
 		</head>
 		<body>
 			<?php 
-				echo '<h1><strong>Teste da Linguagem HTML (HyperText Markup Language)</strong></h1>' <br>;
-				echo '<strong>Autor: Iann Oliveira</strong><br>';
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Iann Oliveira<br>';
 				echo 'Linkedin: linkedin.com/in/iann-oliveira-3106b11a4/<br>';
 				echo 'Site: github.com/iannoliver<br>';
 				echo 'Instagram: instagram.com/oli_iann/<br>';
 				echo 'YouTube: youtube.com/channel/UCrFED4DHwF8EVJ5yR90OXbA<br>';
 				echo '<img src="download.jpg" alt="">';
-				<br> 
 			?>
 		</body>
 	</html>
